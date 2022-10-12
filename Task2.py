3abead53385c0c2454bbe44d98e15d420ebd7098
Bulavina Elena <koroleva488@mail.ru>
2022-10-12 19:18:28 +1000

    Задача 2 (коррект.)

--- Task2.py
+++ Task2.py
@@ -5,10 +5,15 @@
 #[2, 3, 4, 5, 6] => [12, 15, 16]
 #[2, 3, 5, 6] => [12, 15]
 
-result=0
-numbers=[1, 5, 8, 15]
-for i in range(len(numbers)):
-    result=numbers[i]*numbers[-1]
-    print(result)
-
-
+from random import randint as rI
+numbers=int(input('Введите размер списка: '))
+firstList=[]
+for i in range(numbers):
+    firstList.append(rI(-15, 15))
+print(firstList)
+listLenght=len(firstList)
+resultList=[]
+for i in range(listLenght//2+1):
+    element=firstList[i]*firstList[listLenght-i-1]
+    resultList.append(element)
+print(resultList)
