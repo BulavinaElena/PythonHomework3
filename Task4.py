c35e3501ee2a2bbf7a9dbf417354519d88895de6
Bulavina Elena <koroleva488@mail.ru>
2022-10-12 19:53:46 +1000

    Задача 4

---
+++ Task4.py
@@ -0,0 +1,17 @@
+# Задача 4. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+#Пример:
+#45 => 101101
+#3 => 11
+#2 => 10
+
+import numbers
+
+
+number=int(input('Введите десятичное число: '))
+num=number
+binaryNumber=[]
+while number>0:
+    binaryNumber.insert(0, str(number%2))
+    number//=2
+print(f"Число {num} в двоичной системе {''.join(binaryNumber)}")
